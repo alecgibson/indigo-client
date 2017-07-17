0c087ec6ab847c7acf261279337fec2791b6d9c8
Alec Gibson <aleckgibson@googlemail.com>
2017-07-17 08:17:30 +0100

    Opponent stat bar

--- source/components/battle/Arena.js
+++ source/components/battle/Arena.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Image, StyleSheet, View} from "react-native";
+import {Image, StyleSheet, Text, View} from "react-native";
 import Sprites from "../sprites/sprites";
 import PropTypes from "prop-types";
 
@@ -31,6 +31,30 @@ export default class Arena extends Component {
           style={styles.ownPokemon}
           source={Sprites.sprites.pokemon.battle[this.ownSpeciesId()]['back']}
         />
+        <View style={styles.opponentStatBarContainer}>
+          <Image
+            style={styles.statBar}
+            source={require('../../../assets/images/battle/stat-bar-opponent.png')}
+          />
+          <View style={[styles.healthBar, styles.opponentHealthBar]}>
+            <Image
+              style={{height: '100%', width: this.asPercentage(this.props.battle.opponentPokemon.hitPointFraction)}}
+              source={this.healthBar(this.props.battle.opponentPokemon.hitPointFraction)}
+            />
+          </View>
+          <Text style={[styles.statText, styles.opponentPokemonName]}>
+            {this.props.battle.opponentPokemon.name}
+          </Text>
+          <Text style={[styles.statText, styles.opponentPokemonLevel]}>
+            {this.props.battle.opponentPokemon.level}
+          </Text>
+        </View>
+        <View style={styles.ownStatBarContainer}>
+          <Image
+            style={styles.statBar}
+            source={require('../../../assets/images/battle/stat-bar-own.png')}
+          />
+        </View>
       </View>
     );
   }
@@ -45,6 +69,20 @@ export default class Arena extends Component {
 
   zeroPad(number) {
     return ('000' + number).slice(-3);
+  }
+
+  asPercentage(decimal) {
+    return decimal * 100 + '%';
+  }
+
+  healthBar(hitPointFraction) {
+    if (hitPointFraction > 0.5) {
+      return require('../../../assets/images/battle/health-bar-green.png');
+    } else if (hitPointFraction > 0.2) {
+      return require('../../../assets/images/battle/health-bar-yellow.png');
+    } else {
+      return require('../../../assets/images/battle/health-bar-red.png');
+    }
   }
 }
 
@@ -90,5 +128,51 @@ const styles = StyleSheet.create({
     bottom: '-5%',
     width: '70%',
     height: '60%',
+  },
+  opponentStatBarContainer: {
+    position: 'absolute',
+    left: '0%',
+    top: '10%',
+    width: '40%',
+    height: '30%',
+  },
+  healthBar: {
+    position: 'absolute',
+    height: '2.75%',
+    width: '39.5%',
+  },
+  opponentHealthBar: {
+    bottom: '27.5%',
+    left: '35%',
+  },
+  statText: {
+    fontFamily: 'Thintel',
+    fontSize: 24,
+    color: 'white',
+    textShadowColor: 'black',
+    textShadowOffset: {width: 1, height: 0},
+    textShadowRadius: 3,
+  },
+  opponentPokemonName: {
+    position: 'absolute',
+    bottom: '29%',
+    left: '5%',
+  },
+  opponentPokemonLevel: {
+    position: 'absolute',
+    bottom: '29%',
+    left: '75%',
+  },
+  ownStatBarContainer: {
+    position: 'absolute',
+    right: '0%',
+    bottom: '10%',
+    width: '40%',
+    height: '40%',
+  },
+  statBar: {
+    resizeMode: 'contain',
+    width: '100%',
+    height: '100%',
   },
 });
